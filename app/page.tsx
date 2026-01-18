8210a1342d8c111ea3bc4a3605c2f3fae81dc5bc
unknown <jgu13@illinois.edu>
2026-01-18 01:23:18 -0600

    'init'

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,20 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full  flex-col items-center justify-between py-8 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/logo.png"
           alt="Next.js logo"
-          width={100}
-          height={20}
+          width={800}
+          height={500}
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            Omega-3 Fatty Acids: Essential Nutrients for Brain and Heart Health
           </h1>
+
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
