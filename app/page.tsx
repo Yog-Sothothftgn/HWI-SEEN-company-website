12230acf3e5e93a7d880e7c38fa559d9c35e552f
unknown <jgu13@illinois.edu>
2026-01-19 02:53:09 -0600

    creating sections

--- app/page.tsx
+++ app/page.tsx
@@ -2,64 +2,57 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full  flex-col items-center justify-between py-8 px-16 bg-[#EDECE9] dark:bg-black sm:items-start">
-        <div className="w-full max-w-3xl px-6 mx-auto mt-10 mb-16">
-          <Image
-            src="/logo_transparent.png"
-            alt="HWI SENN"
-            width={1200}        
-            height={1200}
-            className="mx-auto w-full h-auto"
-            priority
-          />
-        </div>
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            Omega-3 Fatty Acids: Essential Nutrients for Brain and Heart Health
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+    <div className="relative flex min-h-screen items-center justify-center bg-gradient-to-br from-[#EDECE9] via-[#E2E2E2] to-[#DEDFE0] font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full  flex-col items-center justify-between sm:items-start">
+        <div className="h-screen w-screen overflow-y-scroll snap-y snap-mandatory justify-between scroll-smooth">
+          <section className="h-screen snap-start flex items-center justify-center bg-[#E2E2E2] text-[#66ccff]">
+            <div className="w-full max-w-4xl items-center px-6 mx-auto">
+              <Image
+                src="/logo_transparent.png"
+                alt="HWI SENN"
+                width={1400}        
+                height={1400}
+                className="mx-auto w-full h-auto"
+                priority
+              />
+            </div>
+            <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+              <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+                Omega-3 Fatty Acids: Essential Nutrients for Brain and Heart Health
+              </h1>
+              <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+                Looking for more information or need to contact us? Head over to{" "}
+                <a
+                  href="https://www.linkedin.com/in/shelley-jiang-1a2b2337/"
+                  className="font-medium text-zinc-950 dark:text-zinc-50"
+                >
+                  linkin
+                </a>{" "}
+                or the{" "}
+                <a
+                  href="gujiaming2004@outlook.com"
+                  className="font-medium text-zinc-950 dark:text-zinc-50"
+                >
+                  Email
+                </a>{" "}for more details.
+              </p>
+            </div>
+          </section>
+
+          <section className="h-screen snap-start flex items-center justify-center bg-[#DEDFE0] text-[#66ccff]">
+            <h2 className="text-4xl font-semibold">Section 2</h2>
+          </section>
+
+          <section className="h-screen snap-start flex items-center justify-center bg-[#DEDFE0] text-[#66ccff]">
+            <h2 className="text-4xl font-semibold">Section 3</h2>
+          </section>
+
+          <section className="h-screen snap-start flex items-center justify-center bg-[#DEDFE0] text-[#66ccff]">
+            <h2 className="text-4xl font-semibold">Section 4</h2>
+          </section>
+          <section className="h-screen snap-start flex items-center justify-center bg-[#DEDFE0] text-[#66ccff]">
+            <h2 className="text-4xl font-semibold">Section 5</h2>
+          </section>
         </div>
       </main>
     </div>
